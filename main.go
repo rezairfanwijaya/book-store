d8270b8b98b5e158179a7c8be7a6659adad779ae
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-06-07 22:54:38 +0700

    add: endpoint login admin

--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"book-store/admin"
 	"book-store/database"
+	"book-store/handler"
 	"log"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// connection
-	_, err := database.NewConnection(".env")
+	connection, err := database.NewConnection(".env")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	log.Println("Success connection")
+	// admin
+	repoAdmin := admin.NewRepository(connection)
+	serviceAdmin := admin.NewService(repoAdmin)
+	handlerAdmin := handler.NewHandlerAdmin(serviceAdmin)
+
+	// http server
+	router := gin.Default()
+
+	// api versioning
+	apiV1 := router.Group("api/v1")
+
+	// routing admin
+	apiV1.POST("/admin/login", handlerAdmin.Login)
+
+	// serve
+	if err := router.Run(":5454"); err != nil {
+		log.Fatalf("failed start server : %v", err.Error())
+	}
 }
