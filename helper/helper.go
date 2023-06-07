1dad516aed1db0a31373c2c5043abcd207c8677d
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-06-07 22:17:08 +0700

    add: hash and verify password

--- helper/helper.go
+++ helper/helper.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/joho/godotenv"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func GetENV(path string) (map[string]string, error) {
@@ -13,4 +14,23 @@ func GetENV(path string) (map[string]string, error) {
 	}
 
 	return env, nil
+}
+
+func EcryptPassword(password string) (string, error) {
+	// encrypt password
+	passENC, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	if err != nil {
+		return "", fmt.Errorf("failed encrypt password : %v", err.Error())
+	}
+
+	return string(passENC), nil
+}
+
+func VerifyPassword(rawPassword, passwordHash string) error {
+	// cocokan password input dan password dari db
+	if err := bcrypt.CompareHashAndPassword([]byte(passwordHash), []byte(rawPassword)); err != nil {
+		return fmt.Errorf("wrong password")
+	}
+
+	return nil
 }
