2cf0c066a1bbb650e29f5b59b10a35624e74a2de
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-06-07 22:55:05 +0700

    update: entity

--- admin/entity.go
+++ admin/entity.go
@@ -1,7 +1,7 @@
 package admin
 
 type Admin struct {
-	ID       int    `json:"id gorm:primaryKey"`
+	ID       int    `json:"id" gorm:"primaryKey"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 	Role     string `json:"role"`
