443ef4c790364f3366c79ec9e06570fc9bafc7e6
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-06-07 22:55:36 +0700

    add: service login admin

--- admin/service.go
+++ admin/service.go
@@ -1,5 +1,11 @@
 package admin
 
+import (
+	"book-store/helper"
+	"fmt"
+	"net/http"
+)
+
 type IService interface {
 	Login(input InputAdminLogin) (Admin, int, error)
 }
@@ -10,4 +16,25 @@ type service struct {
 
 func NewService(repoAdmin IRepository) *service {
 	return &service{repoAdmin}
+}
+
+func (s *service) Login(input InputAdminLogin) (Admin, int, error) {
+	// cari email yang diinput
+	adminByEmail, err := s.repoAdmin.FindByEmail(input.Email)
+	if err != nil {
+		return adminByEmail, http.StatusInternalServerError, err
+	}
+
+	// jika email salah
+	if adminByEmail.ID == 0 {
+		return adminByEmail, http.StatusBadRequest, fmt.Errorf("email %v tidak terdaftar", input.Email)
+	}
+
+	// jika password salah
+	if err := helper.VerifyPassword(input.Password, adminByEmail.Password); err != nil {
+		return adminByEmail, http.StatusBadRequest, err
+	}
+
+	// sukses login
+	return adminByEmail, http.StatusOK, nil
 }
