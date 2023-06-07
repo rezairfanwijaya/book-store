4a16dd20baba2e76691eb836a7e086d4a7a6b1d0
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-06-07 22:56:54 +0700

    add: api and error formatter

--- helper/helper.go
+++ helper/helper.go
@@ -3,9 +3,34 @@ package helper
 import (
 	"fmt"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/joho/godotenv"
 	"golang.org/x/crypto/bcrypt"
 )
+
+type response struct {
+	Meta meta        `json:"meta"`
+	Data interface{} `json:"data"`
+}
+
+type meta struct {
+	Code   int    `json:"code"`
+	Status string `json:"status"`
+}
+
+func ResponseAPI(status string, code int, data interface{}) *response {
+	meta := meta{
+		Code:   code,
+		Status: status,
+	}
+
+	responseAPI := response{
+		Meta: meta,
+		Data: data,
+	}
+
+	return &responseAPI
+}
 
 func GetENV(path string) (map[string]string, error) {
 	env, err := godotenv.Read(path)
@@ -29,8 +54,19 @@ func EcryptPassword(password string) (string, error) {
 func VerifyPassword(rawPassword, passwordHash string) error {
 	// cocokan password input dan password dari db
 	if err := bcrypt.CompareHashAndPassword([]byte(passwordHash), []byte(rawPassword)); err != nil {
-		return fmt.Errorf("wrong password")
+		return fmt.Errorf("password salah")
 	}
 
 	return nil
+}
+
+// format error
+func ErrorFormater(err error) []string {
+	var myError []string
+
+	for _, e := range err.(validator.ValidationErrors) {
+		myError = append(myError, e.Error())
+	}
+
+	return myError
 }
