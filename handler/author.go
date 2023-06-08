6a8d1907c8e9548eb8b217c51e639f6bf0110cc7
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-06-08 11:47:04 +0700

    add: hander register and login author

---
+++ handler/author.go
@@ -0,0 +1,111 @@
+package handler
+
+import (
+	"book-store/auth"
+	"book-store/author"
+	"book-store/helper"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type handlerAuthor struct {
+	authorService author.IService
+	authService   auth.Service
+}
+
+func NewHandlerAuthor(authorService author.IService, authService auth.Service) *handlerAuthor {
+	return &handlerAuthor{authorService, authService}
+}
+
+func (h *handlerAuthor) Register(c *gin.Context) {
+	// binding
+	var input author.InputAuthorSession
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		errBinding := helper.ErrorFormater(err)
+		response := helper.ResponseAPI(
+			"failed",
+			http.StatusBadRequest,
+			errBinding,
+		)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	// service
+	authorRegistered, httpCode, err := h.authorService.Register(input)
+	if err != nil {
+		response := helper.ResponseAPI(
+			"failed",
+			httpCode,
+			err.Error(),
+		)
+
+		c.JSON(httpCode, response)
+		return
+	}
+
+	response := helper.ResponseAPI(
+		"success",
+		httpCode,
+		authorRegistered,
+	)
+
+	c.JSON(httpCode, response)
+}
+
+func (h *handlerAuthor) Login(c *gin.Context) {
+	// binding
+	var input author.InputAuthorSession
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		errBinding := helper.ErrorFormater(err)
+		response := helper.ResponseAPI(
+			"failed",
+			http.StatusBadRequest,
+			errBinding,
+		)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	// service
+	authorLoggedin, httpCode, err := h.authorService.Login(input)
+	if err != nil {
+		response := helper.ResponseAPI(
+			"failed",
+			httpCode,
+			err.Error(),
+		)
+
+		c.JSON(httpCode, response)
+		return
+	}
+
+	// generate token jwt
+	token, err := h.authService.GenerateToken(authorLoggedin.ID)
+	if err != nil {
+		response := helper.ResponseAPI(
+			"failed generate token",
+			httpCode,
+			err.Error(),
+		)
+
+		c.JSON(httpCode, response)
+		return
+	}
+
+	// format admin
+	authorFormatted := author.FormatterAuthorLogin(authorLoggedin, token)
+
+	response := helper.ResponseAPI(
+		"success",
+		httpCode,
+		authorFormatted,
+	)
+
+	c.JSON(httpCode, response)
+}
