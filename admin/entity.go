af50bb21eb4f8ea4551d3ee5012d325e279a7d75
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-06-08 11:48:10 +0700

    remove: admin domain

--- admin/entity.go
+++
@@ -1,8 +0,0 @@
-package admin
-
-type Admin struct {
-	ID       int    `json:"id" gorm:"primaryKey"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Role     string `json:"role"`
-}
