1989409cd7d5b8f643f285913fdba0570bb6fabe
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-06-09 00:04:33 +0700

    add: query get all books

--- book/repository.go
+++ book/repository.go
@@ -9,6 +9,8 @@ import (
 type IRepository interface {
 	Save(book entity.Book) (entity.Book, error)
 	FindByBookTitle(bookTitle string) (entity.Book, error)
+	FindAll() ([]entity.Book, error)
+	Update(book entity.Book) (entity.Book, error)
 }
 
 type repository struct {
@@ -30,6 +32,23 @@ func (r *repository) Save(book entity.Book) (entity.Book, error) {
 func (r *repository) FindByBookTitle(bookTitle string) (entity.Book, error) {
 	var book entity.Book
 	if err := r.db.Model(&entity.Book{}).Preload("Authors").Where("title = ?", bookTitle).Find(&book).Error; err != nil {
+		return book, err
+	}
+
+	return book, nil
+}
+
+func (r *repository) FindAll() ([]entity.Book, error) {
+	var books []entity.Book
+	if err := r.db.Model(&entity.Book{}).Preload("Authors").Find(&books).Error; err != nil {
+		return books, err
+	}
+
+	return books, nil
+}
+
+func (r *repository) Update(book entity.Book) (entity.Book, error) {
+	if err := r.db.Save(&book).Error; err != nil {
 		return book, err
 	}
 
