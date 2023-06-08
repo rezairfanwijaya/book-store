fe69e0551b0d4898cfca2e8293d46da79c1b3585
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-06-09 00:43:39 +0700

    add: handler update book

--- handler/book.go
+++ handler/book.go
@@ -18,7 +18,7 @@ func NewHandlerBook(bookService book.IService) *handlerBook {
 }
 
 func (h *handlerBook) Create(c *gin.Context) {
-	// ambil infor author yang create new book
+	// ambil info author yang create new book
 	currentAuthor := c.MustGet("currentAuthor").(entity.Author)
 
 	// input binding
@@ -81,6 +81,62 @@ func (h *handlerBook) GetAll(c *gin.Context) {
 		"success",
 		httpCode,
 		booksFormatted,
+	)
+
+	c.JSON(httpCode, response)
+}
+
+func (h *handlerBook) Update(c *gin.Context) {
+	title := c.Param("title")
+
+	if title == "" {
+		response := helper.ResponseAPI(
+			"failed",
+			http.StatusBadRequest,
+			"param not be empty",
+		)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	// ambil info author yang create new book
+	currentAuthor := c.MustGet("currentAuthor").(entity.Author)
+
+	// input binding
+	var input book.InputUpdateBook
+	input.Author = currentAuthor
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		errBinding := helper.ErrorFormater(err)
+		response := helper.ResponseAPI(
+			"failed",
+			http.StatusBadRequest,
+			errBinding,
+		)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	// service
+	bookUpdated, httpCode, err := h.bookService.Update(input, title)
+	if err != nil {
+		response := helper.ResponseAPI(
+			"failed",
+			httpCode,
+			err.Error(),
+		)
+
+		c.JSON(httpCode, response)
+		return
+	}
+
+	// formatter
+	bookUpdatedFormatted := book.FormatterResponseBook(bookUpdated)
+	response := helper.ResponseAPI(
+		"success",
+		httpCode,
+		bookUpdatedFormatted,
 	)
 
 	c.JSON(httpCode, response)
