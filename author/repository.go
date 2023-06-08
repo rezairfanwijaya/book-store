679f30b96cf49715cbce173dd4c47f7e0a71dd73
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-06-08 12:20:41 +0700

    add: requery to fin author by id

--- author/repository.go
+++ author/repository.go
@@ -5,6 +5,7 @@ import "gorm.io/gorm"
 type IRepository interface {
 	Save(author Author) (Author, error)
 	FindByName(name string) (Author, error)
+	FindByID(id int) (Author, error)
 }
 
 type repository struct {
@@ -26,6 +27,15 @@ func (r *repository) Save(author Author) (Author, error) {
 func (r *repository) FindByName(name string) (Author, error) {
 	var author Author
 	if err := r.db.Where("name = ?", name).Find(&author).Error; err != nil {
+		return author, err
+	}
+
+	return author, nil
+}
+
+func (r *repository) FindByID(id int) (Author, error) {
+	var author Author
+	if err := r.db.Where("id = ?", id).Find(&author).Error; err != nil {
 		return author, err
 	}
 
