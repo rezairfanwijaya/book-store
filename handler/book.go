af28194fb8de47b44e09582e0a08cb3c5e92e941
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-06-08 13:36:33 +0700

    ad: handler create book

---
+++ handler/book.go
@@ -0,0 +1,62 @@
+package handler
+
+import (
+	"book-store/author"
+	"book-store/book"
+	"book-store/helper"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type handlerBook struct {
+	bookService book.IService
+}
+
+func NewHandlerBook(bookService book.IService) *handlerBook {
+	return &handlerBook{bookService}
+}
+
+func (h *handlerBook) Create(c *gin.Context) {
+	// ambil infor author yang create new book
+	currentAuthor := c.MustGet("currentAuthor").(author.Author)
+
+	// input binding
+	var input book.InputNewBook
+	input.Author = currentAuthor
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		errBinding := helper.ErrorFormater(err)
+		response := helper.ResponseAPI(
+			"failed",
+			http.StatusBadRequest,
+			errBinding,
+		)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	// service
+	bookSaved, httpCode, err := h.bookService.Save(input)
+	if err != nil {
+		response := helper.ResponseAPI(
+			"failed",
+			httpCode,
+			err.Error(),
+		)
+
+		c.JSON(httpCode, response)
+		return
+	}
+
+	// formatting
+	bookSavedFormatted := book.FormatterResponseNewBook(bookSaved)
+
+	response := helper.ResponseAPI(
+		"success",
+		httpCode,
+		bookSavedFormatted,
+	)
+
+	c.JSON(httpCode, response)
+}
