2208a3f27261d700c585cda14a950e8c4f604f42
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-06-08 12:20:19 +0700

    add: middleware

--- main.go
+++ main.go
@@ -5,8 +5,12 @@ import (
 	"book-store/author"
 	"book-store/database"
 	"book-store/handler"
+	"book-store/helper"
 	"log"
+	"net/http"
+	"strings"
 
+	"github.com/dgrijalva/jwt-go/v4"
 	"github.com/gin-gonic/gin"
 )
 
@@ -38,5 +42,59 @@ func main() {
 	// serve
 	if err := router.Run(":5454"); err != nil {
 		log.Fatalf("failed start server : %v", err.Error())
+	}
+}
+
+func authMiddleware(authService auth.Service, authorService author.IService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// ambil nilai header yang sudah kita set namanya Authorization
+		authHeader := c.GetHeader("Authorization")
+
+		// cek apakah nilai authorization memiliki Bearer
+		// karena nanti kita akan set nilai token seperti ini "Bearer djfkfbnfkjbnfkjgbnfkyreguryhvbfdhvbfbvdhbf"
+		if !strings.Contains(authHeader, "Bearer") {
+			respons := helper.ResponseAPI("Unauthorized", http.StatusUnauthorized, nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, respons)
+			return
+		}
+
+		// lalu kita pisahkan tokennya berdasarkan spasi
+		// before "Bearer djfkfbnfkjbnfkjgbnfkyreguryhvbfdhvbfbvdhbf"
+		// after ["Bearer"] ["djfkfbnfkjbnfkjgbnfkyreguryhvbfdhvbfbvdhbf"]
+		tokenString := ""
+		arraytoken := strings.Split(authHeader, " ")
+		if len(arraytoken) == 2 {
+			tokenString = arraytoken[1]
+		}
+
+		// validasi token
+		token, err := authService.ValidasiToken(tokenString)
+		if err != nil {
+			respons := helper.ResponseAPI("Unauthorized", http.StatusUnauthorized, nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, respons)
+			return
+		}
+
+		// ambil data dalam token
+		claim, ok := token.Claims.(jwt.MapClaims)
+		if !ok || !token.Valid {
+			respons := helper.ResponseAPI("Unauthorized", http.StatusUnauthorized, nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, respons)
+			return
+		}
+
+		// data id user diambil
+		userId := int(claim["user_id"].(float64))
+
+		// data user diambil berdasarkan id
+		user, _, err := authorService.GetByID(userId)
+		if err != nil {
+			respons := helper.ResponseAPI("Unauthorized", http.StatusUnauthorized, nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, respons)
+			return
+		}
+
+		// set context
+		c.Set("currentUser", user)
 	}
 }
